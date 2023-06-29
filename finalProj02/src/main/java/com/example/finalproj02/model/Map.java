fe195c8988aa2db288413cc6d943297ada03d37e
hanaNrvtn <nrvtnhana@gmail.com>
2023-06-29 15:24:15 -0700

    Player.java and Map.java: other model classes added. Multiple constructors supports all kinds of instance making of them, needed while transferring data between GUI and database.

---
+++ finalProj02/src/main/java/com/example/finalproj02/model/Map.java
@@ -0,0 +1,35 @@
+package com.example.finalproj02.model;
+
+import java.util.ArrayList;
+
+public class Map {
+    private int mapID;
+    private int playerID;
+    private final ArrayList<Building> buildings;
+    public Map(ArrayList<Building> buildings){
+        this.buildings=buildings;
+    }
+
+    public Map(int playerID, ArrayList<Building> buildings){
+        this.playerID=playerID;
+        this.buildings=buildings;
+    }
+
+    public Map(int mapID, int playerID, ArrayList<Building> buildings){
+        this.mapID=mapID;
+        this.playerID=playerID;
+        this.buildings=buildings;
+    }
+
+    public int getMapID() {
+        return mapID;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public ArrayList<Building> getBuildings() {
+        return buildings;
+    }
+}
---
+++ finalProj02/src/main/java/com/example/finalproj02/model/Player.java
@@ -0,0 +1,95 @@
+package com.example.finalproj02.model;
+
+public class Player {
+    private int playerID;
+    private String nickname;
+    private final String username;
+    private String password;
+    private int level;
+    private int wonBattles;
+    private int lostBattles;
+    private Map map;
+
+    public Player(int playerID, String username, String nickname, String password, int level, int wonBattles, int lostBattles, Map map){
+        this.playerID=playerID;
+        this.username=username;
+        this.nickname=nickname;
+        this.password=password;
+        this.wonBattles=wonBattles;
+        this.lostBattles=lostBattles;
+        this.level=level;
+        this.map=map;
+    }
+
+    public Player(String username, String password, int level, int wonBattles, int lostBattles){
+        this.username=username;
+        this.password=password;
+        this.wonBattles=wonBattles;
+        this.lostBattles=lostBattles;
+        this.level=level;
+    }
+
+    public Player(int playerID, String username, String nickname, String password, int level, int wonBattles, int lostBattles){
+        this.username=username;
+        this.password=password;
+        this.wonBattles=wonBattles;
+        this.lostBattles=lostBattles;
+        this.level=level;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getWonBattles() {
+        return wonBattles;
+    }
+
+    public void setWonBattles(int wonBattles) {
+        this.wonBattles = wonBattles;
+    }
+
+    public int getLostBattles() {
+        return lostBattles;
+    }
+
+    public void setLostBattles(int lostBattles) {
+        this.lostBattles = lostBattles;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+}
